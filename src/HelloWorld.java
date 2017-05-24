5f3d155dde2adc32185b2b36ed4c45eedf71138c
Saurav Sharma <sauravsharma011@gmail.com>
2017-05-24 13:26:30 -0400

    first commit  of the project

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	
+	/* This is a multi-line comment 
+	 * Author:Saurav Sharma
+	 * Program Description: THis program prints out hello World!"
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World!");
+	}
+
+}
